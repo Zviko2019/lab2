1b15665a973929d733a4bbdecd3f9909613fe98f
Zviko2019 <zchirozvi17@gmail.com>
2019-08-14 23:02:04 +0200

    size_capacity

--- Desktop/elen3009-lab2/src/vector/size_capacity.cpp
+++ Desktop/elen3009-lab2/src/vector/size_capacity.cpp
@@ -1,3 +1,4 @@
+
 // size_capacity.cpp
 // Vector size versus capacity
 
@@ -8,15 +9,19 @@ using namespace std;
 
 int main()
 {
-    vector<int> vec;
-    cout << "vec: size: " << vec.size() 
-         << " capacity: " << vec.capacity() << endl;
+	vector<int> vec;
+	cout << "vec: size: " << vec.size()
+	<< " capacity: " << vec.capacity() << endl;
 
-    for(int i = 0; i < 24; i++) {
-        vec.push_back(i);
-        cout << "vec: size: " << vec.size() 
-             << " capacity: " << vec.capacity() << endl;
-    }
+	for(int i = 0; i < 24; i++) {
+		vec.push_back(i);
+		cout << "vec: size: " << vec.size()
+		<< " capacity: " << vec.capacity() << endl;
+	}
 
-    return 0;
+	return 0;
 }
+
+///size has to do with number of elements in the vector.
+///capacity of a vector refers to the memory space allocated to a vector depending on the size of the vector.This leaves room of growth(in terms of number of elements of the vector.)
+///memory can be reallocated if no extra memory is needed or allocated if extra memory is needed. Max_size gives the maximum number of elements that can stored in the currently allocated memory space.
