5e5cf8757035a8989d04fbf634c6437b7628f700
Zviko2019 <zchirozvi17@gmail.com>
2019-08-15 22:20:31 +0200

    factorial

--- Desktop/elen3009-lab2/src/factorial/factorial.cpp
+++ Desktop/elen3009-lab2/src/factorial/factorial.cpp
@@ -1,12 +1,16 @@
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+int factorial(int number) { return number >=1 ? factorial(number - 1) * number: 1; }
 
 TEST_CASE("testing the factorial function") {
-    CHECK(factorial(0) == 1);
-    CHECK(factorial(1) == 1);
-    CHECK(factorial(2) == 2);
-    CHECK(factorial(3) == 6);
-    CHECK(factorial(10) == 3628800);
+	CHECK(factorial(0) == 1);
+	CHECK(factorial(1) == 1);
+	CHECK(factorial(2) == 2);
+	CHECK(factorial(3) == 6);
+	CHECK(factorial(10) == 3628800);
+	CHECK(factorial(11) == 39916800);
 }
+
+
